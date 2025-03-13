b05fe9af5476a4abc8f5469178d4d88170288515
amank1412 <amankumarr7390@gmail.com>
2025-03-13 23:34:05 +0530

    add files

--- Lecture 01/Lecture 02/firstProgram.cpp
+++ Lecture 01/Lecture 02/firstProgram.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"Namaste Dunia"<<endl;
-    return 0;
+
+int main() {
+    
+  cout << " Namaste Dunia " << endl;
+  
+  return 0; 
 }
